ddf164ddb9cbcea3b7f1d3264f5304d68be908cc
leopoldguyot <leopold.guyot13@gmail.com>
2025-12-04 13:33:04 +0100

    starting day 3 (part 2 wrong)

--- day3/Main.java
+++ day3/Main.java
@@ -1,8 +1,52 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.*;
+import java.util.*;
+
 public class Main {
     static long cumsum; 
+
+    public static String digitsToString(int[] arr) {
+        StringBuilder sb = new StringBuilder(arr.length);
+        for (int x : arr) sb.append(x);
+        return sb.toString();
+    }
+
+    public static int[] removeValueAtIndex(int[] arr, int index) {
+        int[] newArr = new int[arr.length - 1];
+        int idx = 0;
+        for (int i = 0; i <= arr.length - 1; i++) {
+            if (i != index) {
+                newArr[idx] = arr[i];
+                idx++;
+            }
+        }
+        return (newArr);
+    }
+
+    public static int findIndexMin(int[] nums) {
+        int minAt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            minAt = nums[i] < nums[minAt] ? i : minAt;
+        }
+        return (minAt);
+    }
+    public static long findMaxJoltPart2(String bank) {
+        String[] splitted = bank.split("");
+        int[] jolts = new int[splitted.length];
+
+        for (int i = 0; i <= splitted.length - 1; i++) {
+            jolts[i] = Integer.parseInt(splitted[i]);
+        }
+
+        while (jolts.length > 12) {
+            jolts = removeValueAtIndex(jolts, findIndexMin(jolts));
+        }
+        
+        System.out.println(digitsToString(jolts));
+        return(Long.parseLong(digitsToString(jolts)));
+    }
 
     public static int findMaxJolt(String bank) {
         String[] splitted = bank.split("");
@@ -29,10 +73,11 @@ public class Main {
     public static void main(String[] args) {
         BufferedReader reader;
         try {
-            reader = new BufferedReader(new FileReader("input.txt"));
+            reader = new BufferedReader(new FileReader("input_day3.txt"));
             String line = reader.readLine();
             while (line != null) {
                 cumsum += findMaxJolt(line);
+                System.out.println(findMaxJolt(line));
                 line = reader.readLine();
             }
             reader.close();
---
+++ day3/day_3.py
@@ -0,0 +1,23 @@
+f = open("input_day3.txt")
+
+def charge_batteries(joltage,nb):
+    maxind = 0
+    minind = 1
+    for j in range(0,len(joltage)-1) :
+        if joltage[j] > joltage[maxind] : 
+            maxind = j
+    for i in range(maxind+1, len(joltage)):
+        if joltage[i] > joltage[minind] :
+            minind = i 
+    number_str = str(joltage[maxind])+str(joltage[minind])
+    print(number_str)
+    nb += int(number_str)
+    return(nb)
+
+nbTot=0
+largest=0
+for line in f.readlines():
+    test = line.strip()
+    largest= charge_batteries(test,largest)
+print(largest)
+
